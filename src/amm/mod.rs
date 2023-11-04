e409aeea1bdeb1df2baa32a2049a48c7bebaadda
Dimis Tsaousis <dimistsaousis@gmail.com>
2023-11-04 10:35:52 +0200

    UniswapV2Pool struct

---
+++ src/amm/mod.rs
@@ -0,0 +1,14 @@
+use ethers::types::H160;
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Clone, Default, Serialize, Deserialize)]
+pub struct UniswapV2Pool {
+    pub address: H160,
+    pub token_a: H160,
+    pub token_a_decimals: u8,
+    pub token_b: H160,
+    pub token_b_decimals: u8,
+    pub reserve_0: u128,
+    pub reserve_1: u128,
+    pub fee: u32,
+}
