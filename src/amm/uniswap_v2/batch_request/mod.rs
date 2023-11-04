6bc84b73a8738aa272a40437133f0880d5228683
Dimis Tsaousis <dimistsaousis@gmail.com>
2023-11-04 10:57:14 +0200

    get_pool_from_tokens

--- src/amm/uniswap_v2/batch_request/mod.rs
+++ src/amm/uniswap_v2/batch_request/mod.rs
@@ -3,37 +3,15 @@ use ethers::types::H160;
 
 use crate::amm::UniswapV2Pool;
 
-fn get_pool_from_tokens(tokens: Vec<Token>, address: H160, fee: u32) -> UniswapV2Pool {
-    UniswapV2Pool::new(
+fn get_pool_from_tokens(tokens: Vec<Token>, address: H160, fee: u32) -> Option<UniswapV2Pool> {
+    Some(UniswapV2Pool::new(
         address,
-        tokens[0]
-            .to_owned()
-            .into_address()
-            .expect("Could not parse token A for UniswapV2 pool"),
-        tokens[1]
-            .to_owned()
-            .into_uint()
-            .expect("Could not parse token A decimals for UniswapV2 pool")
-            .as_u32() as u8,
-        tokens[2]
-            .to_owned()
-            .into_address()
-            .expect("Could not parse token B for UniswapV2 pool"),
-        tokens[3]
-            .to_owned()
-            .into_uint()
-            .expect("Could not parse token B decimals for UniswapV2 pool")
-            .as_u32() as u8,
-        tokens[4]
-            .to_owned()
-            .into_uint()
-            .expect("Could not parse reserve 0 for UniswapV2 pool")
-            .as_u128(),
-        tokens[5]
-            .to_owned()
-            .into_uint()
-            .expect("Could not parse reserve 1 for UniswapV2 pool")
-            .as_u128(),
+        tokens[0].to_owned().into_address()?,
+        tokens[1].to_owned().into_uint()?.as_u32() as u8,
+        tokens[2].to_owned().into_address()?,
+        tokens[3].to_owned().into_uint()?.as_u32() as u8,
+        tokens[4].to_owned().into_uint()?.as_u128(),
+        tokens[5].to_owned().into_uint()?.as_u128(),
         fee,
-    )
+    ))
 }
