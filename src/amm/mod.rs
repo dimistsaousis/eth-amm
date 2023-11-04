c74f002732c8451cc762799e86c1d9bf8dcb8f5e
Dimis Tsaousis <dimistsaousis@gmail.com>
2023-11-04 10:46:34 +0200

    uniswap v2 pool new

--- src/amm/mod.rs
+++ src/amm/mod.rs
@@ -1,3 +1,5 @@
+pub mod uniswap_v2;
+
 use ethers::types::H160;
 use serde::{Deserialize, Serialize};
 
@@ -11,4 +13,29 @@ pub struct UniswapV2Pool {
     pub reserve_0: u128,
     pub reserve_1: u128,
     pub fee: u32,
+}
+
+impl UniswapV2Pool {
+    #[allow(clippy::too_many_arguments)]
+    pub fn new(
+        address: H160,
+        token_a: H160,
+        token_a_decimals: u8,
+        token_b: H160,
+        token_b_decimals: u8,
+        reserve_0: u128,
+        reserve_1: u128,
+        fee: u32,
+    ) -> UniswapV2Pool {
+        UniswapV2Pool {
+            address,
+            token_a,
+            token_a_decimals,
+            token_b,
+            token_b_decimals,
+            reserve_0,
+            reserve_1,
+            fee,
+        }
+    }
 }
