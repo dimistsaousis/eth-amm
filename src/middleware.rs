d7b104e40d2811bf52cc375dd53464e6582393bc
Dimis Tsaousis <dimistsaousis@gmail.com>
2023-11-06 22:52:39 +0200

    websocket on eth provider

--- src/middleware.rs
+++ src/middleware.rs
@@ -1,20 +1,28 @@
 use std::sync::Arc;
 
-use ethers::providers::{Http, Middleware, Provider};
+use ethers::providers::{Http, Middleware, Provider, Ws};
 
 pub struct EthProvider {
-    pub provider: Arc<Provider<Http>>,
+    pub http: Arc<Provider<Http>>,
+    pub wss: Arc<Provider<Ws>>,
 }
 
 impl EthProvider {
-    pub fn new() -> EthProvider {
+    pub async fn new() -> EthProvider {
         let rpc_endpoint = std::env::var("NETWORK_RPC").expect("Could not load env `NETWORK_RPC`");
-        let provider = Arc::new(Provider::<Http>::try_from(rpc_endpoint).unwrap());
-        EthProvider { provider }
+        let http = Arc::new(Provider::<Http>::try_from(rpc_endpoint).unwrap());
+
+        let wss_endpoint = std::env::var("NETWORK_WSS").expect("Could not load env `NETWORK_WSS`");
+        let wss = Arc::new(
+            Provider::<Ws>::connect(wss_endpoint)
+                .await
+                .expect("Could not connect to web socket."),
+        );
+        EthProvider { http, wss }
     }
 
     pub async fn get_block_number(&self) -> u64 {
-        self.provider
+        self.http
             .get_block_number()
             .await
             .expect("Could not get block number from provider.")
