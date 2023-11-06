0f01a763ea07c8a43e19f10c92b8ef5b3993dc6d
Dimis Tsaousis <dimistsaousis@gmail.com>
2023-11-06 20:32:34 +0200

    checkpoint using h160

--- src/checkpoint/mod.rs
+++ src/checkpoint/mod.rs
@@ -3,25 +3,24 @@ use serde::{Deserialize, Serialize};
 use std::fs;
 
 #[derive(Serialize, Deserialize)]
-pub enum CheckpointData {
-    H160Data(H160),
-}
-
-#[derive(Serialize, Deserialize)]
 pub struct Checkpoint {
-    last_block: u64,
-    data: CheckpointData,
+    pub last_block: u64,
+    pub data: Vec<H160>,
 }
 
 impl Checkpoint {
-    pub fn new(last_block: u64, data: CheckpointData) -> Self {
+    pub fn new(last_block: u64, data: Vec<H160>) -> Self {
         Checkpoint { last_block, data }
     }
 
-    pub fn load_data(loc: &str) -> Self {
-        let data = fs::read_to_string(loc).unwrap();
-        let checkpoint: Checkpoint = serde_json::from_str(&data).unwrap();
-        checkpoint
+    pub fn load_data(loc: &str) -> Option<Self> {
+        match fs::read_to_string(loc)
+            .map_err(|e| e.to_string())
+            .and_then(|data| serde_json::from_str(&data).map_err(|e| e.to_string()))
+        {
+            Ok(checkpoint) => checkpoint,
+            Err(_) => None,
+        }
     }
 
     pub fn save_data(&self, loc: &str) {
