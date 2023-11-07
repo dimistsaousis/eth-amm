8a1eef11adb2f0d2aa86fc80f339c7274bf1da67
Dimis Tsaousis <dimistsaousis@gmail.com>
2023-11-07 16:41:04 +0200

    test for getting pair address

--- src/amm/uniswap_v2/factory/tests.rs
+++ src/amm/uniswap_v2/factory/tests.rs
@@ -48,3 +48,21 @@ async fn test_all_pairs_length() {
     let result = factory.all_pairs_length(middleware).await;
     assert!(result > 279_174, "Result: {}", result);
 }
+
+#[tokio::test]
+async fn test_get_pair_address() {
+    let SetupResult(factory, middleware) = setup();
+    let weth = H160::from_str("0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2").unwrap();
+    let usdc = H160::from_str("0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48").unwrap();
+    let a1 = factory
+        .get_pair_address(middleware.clone(), weth, usdc)
+        .await;
+    let a2 = factory
+        .get_pair_address(middleware.clone(), usdc, weth)
+        .await;
+    assert_eq!(a1, a2);
+    assert_eq!(
+        a1,
+        H160::from_str("0xB4e16d0168e52d35CaCD2c6185b44281Ec28C9Dc").unwrap()
+    );
+}
