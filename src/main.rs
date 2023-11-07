7956fef5c22b16757eefaef286274146d5d5b2af
Dimis Tsaousis <dimistsaousis@gmail.com>
2023-11-07 23:25:13 +0200

    update main

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,7 @@
-use std::str::FromStr;
+use std::{collections::HashMap, str::FromStr};
 
 use eth_amm::{
-    amm::{uniswap_v2::factory::UniswapV2Factory, weth_value::get_weth_value_in_pool_concurrent},
-    checkpoint::Checkpoint,
-    middleware::EthProvider,
+    amm::uniswap_v2::factory::UniswapV2Factory, checkpoint::Checkpoint, middleware::EthProvider,
 };
 use ethers::types::{H160, U256};
 
@@ -13,23 +11,14 @@ async fn main() {
     let provider = EthProvider::new().await;
     let factory_address = H160::from_str("0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f").unwrap();
     let factory: UniswapV2Factory = UniswapV2Factory::new(factory_address, 300);
-    let weth = H160::from_str("0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2").unwrap();
-    let pool_addresses =
-        Checkpoint::<H160>::sync_uniswap_v2_pair_addresses(&provider, factory, 100).await;
-    let factory_addresses = vec![factory_address];
-    let weth_values = get_weth_value_in_pool_concurrent(
-        &pool_addresses.data,
-        &factory_addresses,
-        weth,
-        U256::exp10(18),
-        100,
-        provider.http.clone(),
-    )
-    .await;
-    println!(
-        "Got {} pairs with maximum value {:?} and minimum value {:?}",
-        weth_values.len(),
-        weth_values.values().max(),
-        weth_values.values().min()
-    )
+    let Checkpoint {
+        data: weth_values, ..
+    } = Checkpoint::<HashMap<H160, U256>>::sync_uniswap_v2_pools_eth_value(&provider, factory, 100)
+        .await;
+    let min_value = U256::from(10) * U256::exp10(18);
+    let weth_values: HashMap<_, _> = weth_values
+        .iter()
+        .filter(|(_, &value)| &value > &min_value)
+        .collect();
+    println!("Got {} pools with value > 1000 ETH", weth_values.len());
 }
