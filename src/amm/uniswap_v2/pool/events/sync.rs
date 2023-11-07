b6a038a982b7248e9de443d06b189194024824b0
Dimis Tsaousis <dimistsaousis@gmail.com>
2023-11-08 00:52:23 +0200

    capture sync events

---
+++ src/amm/uniswap_v2/pool/events/sync.rs
@@ -0,0 +1,74 @@
+use crate::amm::uniswap_v2::pool::{contracts::SyncFilter, UniswapV2Pool};
+use crate::concurrent::{run_concurrent_hash, BatchError};
+use ethers::prelude::EthEvent;
+use ethers::types::{H160, U256};
+use ethers::{
+    abi::RawLog,
+    providers::Middleware,
+    types::{BlockNumber, Filter, ValueOrArray, H256, U64},
+};
+use indicatif::ProgressBar;
+use std::collections::HashMap;
+use std::sync::{Arc, Mutex};
+
+pub const SYNC_EVENT_SIGNATURE: H256 = H256([
+    28, 65, 30, 154, 150, 224, 113, 36, 28, 47, 33, 247, 114, 107, 23, 174, 137, 227, 202, 180,
+    199, 139, 229, 14, 6, 43, 3, 169, 255, 251, 186, 209,
+]);
+
+impl UniswapV2Pool {
+    async fn get_sync_events_from_logs<'a, M: Middleware + 'a>(
+        start: usize,
+        end: usize,
+        middleware: Arc<M>,
+        progress_bar: Option<Arc<Mutex<ProgressBar>>>,
+    ) -> Result<HashMap<H160, SyncFilter>, BatchError> {
+        let logs = middleware
+            .get_logs(
+                &Filter::new()
+                    .topic0(ValueOrArray::Value(SYNC_EVENT_SIGNATURE))
+                    .from_block(BlockNumber::Number(U64([start as u64])))
+                    .to_block(BlockNumber::Number(U64([end as u64]))),
+            )
+            .await
+            .map_err(|_| BatchError::new(start, end))?;
+
+        let mut sync_events = HashMap::new();
+        let mut last_event: HashMap<H160, (U64, U256)> = HashMap::new();
+        for log in logs {
+            if let (Some(block_number), Some(index)) = (log.block_number, log.log_index) {
+                let address = log.address;
+                let default_value = (U64::from(0), U256::from(0));
+                let (current_block_number, current_index) =
+                    last_event.get(&address).unwrap_or(&default_value);
+                if (&block_number, &index) >= (current_block_number, current_index) {
+                    let sync_event: SyncFilter = SyncFilter::decode_log(&RawLog::from(log))
+                        .map_err(|_| BatchError::new(start, end))?;
+                    last_event.insert(address, (*current_block_number, *current_index));
+                    sync_events.insert(address, sync_event);
+                }
+            }
+        }
+        if let Some(pb) = progress_bar {
+            pb.lock().unwrap().inc(end as u64 - start as u64);
+        }
+        Ok(sync_events)
+    }
+
+    pub async fn get_sync_events_from_logs_concurrent<'a, M: Middleware + 'a>(
+        start: usize,
+        end: usize,
+        step: usize,
+        middleware: Arc<M>,
+    ) -> HashMap<H160, SyncFilter> {
+        let batch_func =
+            |start: usize, end: usize, middleware: Arc<M>, pb: Option<Arc<Mutex<ProgressBar>>>| {
+                Self::get_sync_events_from_logs(start, end, middleware.clone(), pb)
+            };
+        println!(
+            "Getting sync events from logs for Uniswap v2 factory, from block {} to {} with step {}",
+            start, end, step
+        );
+        run_concurrent_hash(start, end, step, middleware, batch_func).await
+    }
+}
