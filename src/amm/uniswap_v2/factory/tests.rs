fde10a441d4ca153db4a6097903c88d4e6370f87
Dimis Tsaousis <dimistsaousis@gmail.com>
2023-11-10 22:25:03 +0200

    update tests to use address book and provider

--- src/amm/uniswap_v2/factory/tests.rs
+++ src/amm/uniswap_v2/factory/tests.rs
@@ -1,25 +1,22 @@
 use super::*;
-use ethers::providers::{Http, Provider};
-use std::str::FromStr;
+use crate::{address_book::AddressBook, middleware::EthProvider};
 
-struct SetupResult(UniswapV2Factory, Arc<Provider<Http>>);
+struct SetupResult(UniswapV2Factory, EthProvider, AddressBook);
 
-fn setup() -> SetupResult {
+async fn setup() -> SetupResult {
     // Create and return the necessary test
     dotenv::dotenv().ok();
-    let address: H160 = H160::from_str("0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f").unwrap();
-    let factory: UniswapV2Factory = UniswapV2Factory { address, fee: 300 };
-    let rpc_endpoint = std::env::var("NETWORK_RPC").unwrap();
-    let middleware = Arc::new(Provider::<Http>::try_from(rpc_endpoint).unwrap());
-
-    SetupResult(factory, middleware)
+    let book = AddressBook::new();
+    let provider = EthProvider::new().await;
+    let factory = UniswapV2Factory::new(book.mainnet.uniswap_v2.factory, 300);
+    SetupResult(factory, provider, book)
 }
 
 #[tokio::test]
 async fn test_get_pair_addresses_from_factory_concurrent_success() {
-    let SetupResult(factory, middleware) = setup();
+    let SetupResult(factory, provider, _) = setup().await;
     let result = factory
-        .get_pair_addresses_from_factory(0, 10, 1, middleware)
+        .get_pair_addresses_from_factory(0, 10, 1, provider.http)
         .await;
     assert_eq!(result.len(), 10);
 }
@@ -27,42 +24,45 @@ async fn test_get_pair_addresses_from_factory_concurrent_success() {
 #[tokio::test]
 #[should_panic]
 async fn test_get_pair_addresses_from_factory_concurrent_failure() {
-    let SetupResult(factory, middleware) = setup();
+    let SetupResult(factory, provider, _) = setup().await;
     factory
-        .get_pair_addresses_from_factory(10_000_000, 10_000_010, 1, middleware)
+        .get_pair_addresses_from_factory(10_000_000, 10_000_010, 1, provider.http)
         .await;
 }
 
 #[tokio::test]
 async fn test_get_pair_addresses_from_logs_success() {
-    let SetupResult(factory, middleware) = setup();
+    let SetupResult(factory, provider, _) = setup().await;
     let result = factory
-        .get_pair_addresses_from_logs_concurrent(10008355, 10009355, 100, middleware)
+        .get_pair_addresses_from_logs_concurrent(10008355, 10009355, 100, provider.http)
         .await;
     assert_eq!(result.len(), 2);
 }
 
 #[tokio::test]
 async fn test_all_pairs_length() {
-    let SetupResult(factory, middleware) = setup();
-    let result = factory.all_pairs_length(middleware).await;
+    let SetupResult(factory, provider, _) = setup().await;
+    let result = factory.all_pairs_length(provider.http).await;
     assert!(result > 279_174, "Result: {}", result);
 }
 
 #[tokio::test]
 async fn test_get_pair_address() {
-    let SetupResult(factory, middleware) = setup();
-    let weth = H160::from_str("0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2").unwrap();
-    let usdc = H160::from_str("0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48").unwrap();
+    let SetupResult(factory, provider, book) = setup().await;
     let a1 = factory
-        .get_pair_address(middleware.clone(), weth, usdc)
+        .get_pair_address(
+            provider.http.clone(),
+            book.mainnet.erc20["weth"],
+            book.mainnet.erc20["usdc"],
+        )
         .await;
     let a2 = factory
-        .get_pair_address(middleware.clone(), usdc, weth)
+        .get_pair_address(
+            provider.http.clone(),
+            book.mainnet.erc20["usdc"],
+            book.mainnet.erc20["weth"],
+        )
         .await;
     assert_eq!(a1, a2);
-    assert_eq!(
-        a1,
-        H160::from_str("0xB4e16d0168e52d35CaCD2c6185b44281Ec28C9Dc").unwrap()
-    );
+    assert_eq!(a1, book.mainnet.uniswap_v2.pairs["weth"]["usdc"]);
 }
