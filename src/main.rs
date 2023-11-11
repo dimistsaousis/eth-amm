7f7482b5fca73f08b2360dd0324386b7d222b166
Dimis Tsaousis <dimistsaousis@gmail.com>
2023-11-11 02:41:11 +0200

    main to run and store simulatons for now

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,42 @@
-use std::str::FromStr;
-
 use eth_amm::{
+    address_book::AddressBook,
     amm::uniswap_v2::{factory::UniswapV2Factory, pool::UniswapV2Pool},
     checkpoint::Checkpoint,
     middleware::EthProvider,
+    pair_paths::{find_pool_paths, get_token_to_pool_map},
+    simulator::{write_simulations_to_csv, Simulation},
 };
-use ethers::types::H160;
+use ethers::types::U256;
 
 #[tokio::main]
 async fn main() {
     dotenv::dotenv().ok();
     let provider = EthProvider::new().await;
-    let factory_address = H160::from_str("0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f").unwrap();
-    let factory: UniswapV2Factory = UniswapV2Factory::new(factory_address, 300);
-    let checkpoint =
-        Checkpoint::<Vec<UniswapV2Pool>>::sync_uniswap_v2_pools(&provider, factory, 100).await;
-    let address = H160::from_str("0xeb1eb49bf534a4b43e47ba2ae7351ac966d29a9a").unwrap();
-    let pool = UniswapV2Pool::from_address(provider.http.clone(), address, 300).await;
-    println!(
-        "{}, {}, {}, {}",
-        pool.reserve_0, pool.reserve_1, pool.token_a_decimals, pool.token_b_decimals
-    );
-
-    let pools: Vec<UniswapV2Pool> = checkpoint
+    let book = AddressBook::new();
+    let factory: UniswapV2Factory = UniswapV2Factory::new(book.mainnet.uniswap_v2.factory, 300);
+    let mut pools_checkpoint = Checkpoint::<Vec<UniswapV2Pool>>::get(&provider, factory, 100).await;
+    pools_checkpoint.sync(&provider).await;
+    // pools_checkpoint
+    //     .sync_eth_value(&provider, book.mainnet.erc20["weth"], U256::exp10(18))
+    //     .await;
+    let pools_before = pools_checkpoint.data.len();
+    let target_pools: Vec<UniswapV2Pool> = pools_checkpoint
         .data
         .into_iter()
-        .filter(|p| p.address == address)
+        .filter(|p| p.eth_value > U256::exp10(19))
         .collect();
-    let pool = &pools[0];
-    println!("{}, {}", pool.reserve_0, pool.reserve_1);
+    println!(
+        "From {} pools, filtered and got {}.",
+        pools_before,
+        target_pools.len()
+    );
+    let token_to_pool_map = get_token_to_pool_map(target_pools);
+    let weth = book.mainnet.erc20["weth"];
+    let paths = find_pool_paths(weth, token_to_pool_map, 4);
+    println!("Found {} possible paths", paths.len());
+    let mut simulations = vec![];
+    for path in paths {
+        simulations.push(Simulation::new(weth, path, U256::exp10(14)));
+    }
+    write_simulations_to_csv(simulations, "simulations.csv");
 }
