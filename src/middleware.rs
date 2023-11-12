358e294be77e7ccd73958cd62b0292f6fe179b80
Dimis Tsaousis <dimistsaousis@gmail.com>
2023-11-12 19:42:03 +0200

    middleware get baalcane, get chain id and get singer middleware

--- src/middleware.rs
+++ src/middleware.rs
@@ -1,6 +1,12 @@
-use std::sync::Arc;
+use std::{str::FromStr, sync::Arc};
 
-use ethers::providers::{Http, Middleware, Provider, Ws};
+use ethers::{
+    abi::Address,
+    middleware::SignerMiddleware,
+    providers::{Http, Middleware, Provider, Ws},
+    signers::LocalWallet,
+    types::{H160, U256},
+};
 
 pub struct EthProvider {
     pub http: Arc<Provider<Http>>,
@@ -21,11 +27,33 @@ impl EthProvider {
         EthProvider { http, wss }
     }
 
+    pub async fn get_balance(&self, address: H160) -> U256 {
+        self.http.get_balance(address, None).await.unwrap()
+    }
+
     pub async fn get_block_number(&self) -> u64 {
         self.http
             .get_block_number()
             .await
             .expect("Could not get block number from provider.")
             .as_u64()
+    }
+
+    pub async fn get_chain_id(&self) -> u64 {
+        self.http
+            .get_chainid()
+            .await
+            .expect("Could not get chain id")
+            .as_u64()
+    }
+
+    pub fn get_signer_middleware(
+        &self,
+        private_key: &str,
+    ) -> Arc<SignerMiddleware<Arc<Provider<Http>>, LocalWallet>> {
+        let wallet = private_key
+            .parse::<LocalWallet>()
+            .expect("Could not parse private key.");
+        Arc::new(SignerMiddleware::new(self.http.clone(), wallet))
     }
 }
