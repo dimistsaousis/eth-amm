f166e001efe78b6cfc567dc16fae449e5ddb2edf
Dimis Tsaousis <dimistsaousis@gmail.com>
2023-11-17 11:49:20 +0200

    update tests to use fixtures for uni v2 pools

--- src/amm/uniswap_v2/pool/tests.rs
+++ src/amm/uniswap_v2/pool/tests.rs
@@ -1,41 +1,33 @@
-use super::*;
-use crate::{address_book::AddressBook, eth_provider::EthProvider};
-use maplit::hashset;
-struct SetupResult(UniswapV2Pool, EthProvider, AddressBook);
+use crate::tests::fixtures;
 
-async fn setup() -> SetupResult {
-    dotenv::dotenv().ok();
-    let book = AddressBook::new();
-    let provider = EthProvider::new_alchemy().await;
-    let pool = UniswapV2Pool::from_address(
-        provider.http.clone(),
-        book.mainnet.uniswap_v2.pairs["weth"]["usdc"],
-        300,
-    )
-    .await;
-    SetupResult(pool, provider, book)
-}
+use super::*;
+use maplit::hashset;
 
 #[tokio::test]
 async fn test_get_reserves() {
-    let SetupResult(pool, provider, _) = setup().await;
-    let (r0, r1) = pool.get_reserves(provider.http).await;
+    let fixture = fixtures::setup().await;
+    let pool = fixture.weth_usdc_uniswap_v2_pool.clone();
+    let http = fixture.alchemy_provider.http.clone();
+    let (r0, r1) = pool.get_reserves(http).await;
     assert_ne!(r0, 0);
     assert_ne!(r1, 0);
 }
 
 #[tokio::test]
 async fn test_sync_reserves() {
-    let SetupResult(mut pool, provider, _) = setup().await;
-    let (r0, r1) = pool.get_reserves(provider.http.clone()).await;
-    pool.sync_reserves(provider.http.clone()).await;
+    let fixture = fixtures::setup().await;
+    let mut pool = fixture.weth_usdc_uniswap_v2_pool.clone();
+    let http = fixture.alchemy_provider.http.clone();
+    let (r0, r1) = pool.get_reserves(http.clone()).await;
+    pool.sync_reserves(http).await;
     assert_eq!(r0, pool.reserve_0);
     assert_eq!(r1, pool.reserve_1);
 }
 
 #[tokio::test]
 async fn test_price() {
-    let SetupResult(pool, _, _) = setup().await;
+    let fixture = fixtures::setup().await;
+    let pool = fixture.weth_usdc_uniswap_v2_pool.clone();
     let price = pool.price(pool.token_b);
     assert!(price < 2500.0);
     assert!(price > 1000.0);
@@ -43,7 +35,8 @@ async fn test_price() {
 
 #[tokio::test]
 async fn test_simulate_swap() {
-    let SetupResult(pool, _, _) = setup().await;
+    let fixture = fixtures::setup().await;
+    let pool = fixture.weth_usdc_uniswap_v2_pool.clone();
     let amount_in = U256::from(1000)
         .checked_mul(U256::from(10).pow(U256::from(pool.token_b_decimals)))
         .unwrap()
@@ -58,17 +51,21 @@ async fn test_simulate_swap() {
 
 #[tokio::test]
 async fn test_get_token_decimals() {
-    let SetupResult(mut pool, provider, _) = setup().await;
-    let (t0, t1) = pool.get_token_decimals(provider.http).await;
+    let fixture = fixtures::setup().await;
+    let mut pool = fixture.weth_usdc_uniswap_v2_pool.clone();
+    let http = fixture.alchemy_provider.http.clone();
+    let (t0, t1) = pool.get_token_decimals(http).await;
     assert_eq!(t0, 6);
     assert_eq!(t1, 18);
 }
 
 #[tokio::test]
 async fn test_get_uniswap_v2_pool_data_concurrent() {
-    let SetupResult(pool, provider, _) = setup().await;
+    let fixture = fixtures::setup().await;
+    let pool = fixture.weth_usdc_uniswap_v2_pool.clone();
+    let http = fixture.alchemy_provider.http.clone();
     let addresses = vec![pool.address];
-    let pools = get_uniswap_v2_pool_data_concurrent(&addresses, provider.http, 300, 1).await;
+    let pools = get_uniswap_v2_pool_data_concurrent(&addresses, http, 300, 1).await;
     let new_pool = pools.into_iter().next().unwrap();
     assert_eq!(pool.address, new_pool.address);
     assert_eq!(pool.token_a, new_pool.token_a);
@@ -79,24 +76,28 @@ async fn test_get_uniswap_v2_pool_data_concurrent() {
 
 #[tokio::test]
 async fn test_factory() {
-    let SetupResult(pool, provider, book) = setup().await;
+    let fixture = fixtures::setup().await;
+    let pool = fixture.weth_usdc_uniswap_v2_pool.clone();
+    let http = fixture.alchemy_provider.http.clone();
     assert_eq!(
-        pool.factory(provider.http).await,
-        book.mainnet.uniswap_v2.factory
+        pool.factory(http).await,
+        fixture.book.mainnet.uniswap_v2.factory
     );
 }
 
 #[tokio::test]
 async fn test_sync_events() {
-    let SetupResult(mut pool, provider, _) = setup().await;
-    pool.sync_reserves(provider.http.clone()).await;
-    let last_block = provider.get_block_number().await;
+    let fixture = fixtures::setup().await;
+    let mut pool = fixture.weth_usdc_uniswap_v2_pool.clone();
+    let http = fixture.alchemy_provider.http.clone();
+    pool.sync_reserves(http.clone()).await;
+    let last_block = fixture.alchemy_provider.get_block_number().await;
     let events = UniswapV2Pool::get_sync_events_from_logs_concurrent(
         (last_block - 100) as usize,
         last_block as usize,
         10,
         hashset![pool.address],
-        provider.http,
+        http,
     )
     .await;
     assert!(events.contains_key(&pool.address));
@@ -107,11 +108,13 @@ async fn test_sync_events() {
 
 #[tokio::test]
 async fn test_sync_pools_from_logs() {
-    let SetupResult(mut pool, provider, _) = setup().await;
+    let fixture = fixtures::setup().await;
+    let mut pool = fixture.weth_usdc_uniswap_v2_pool.clone();
+    let http = fixture.alchemy_provider.http.clone();
     pool.reserve_0 = 0;
     pool.reserve_1 = 0;
     let mut pools = vec![pool];
-    let last_block = provider.get_block_number().await;
+    let last_block = fixture.alchemy_provider.get_block_number().await;
     assert_eq!(&0, &pools[0].reserve_0);
     assert_eq!(&0, &pools[0].reserve_1);
     UniswapV2Pool::sync_pools_from_logs(
@@ -119,10 +122,10 @@ async fn test_sync_pools_from_logs() {
         last_block as usize,
         10,
         &mut pools,
-        provider.http.clone(),
+        http.clone(),
     )
     .await;
-    let (r0, r1) = pools[0].get_reserves(provider.http).await;
+    let (r0, r1) = pools[0].get_reserves(http).await;
     assert_eq!(&r0, &pools[0].reserve_0);
     assert_eq!(&r1, &pools[0].reserve_1);
 }
